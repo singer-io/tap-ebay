d825efe9cef7b49c36daa6b398a1a0f8a0384852
atttiwari <atul.tiwari@qlik.com>
2026-04-01 10:32:43 +0530

    SAC-28705: Upgraded bump version in changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.2.0
+  * Bump dependency versions for singer-python, requests. Added new unittest, intergration test cases.[#12](https://github.com/singer-io/tap-ebay/pull/12)
+
+## 0.1.0
+  * Initial Commit
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-ebay',
-      version='0.1.0',
+      version='0.2.0',
       description='Singer.io tap for extracting data from the Ebay API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
